6990192148634aa912e4b6c75282ad888768cb20
Akshansh Sinha <sinha.akshansh.08@gmail.com>
2025-02-20 07:11:04 +0530

    NavBar Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import AppCss from './App.module.css'
 import LandingPage from "../src/pages/LandingPage"
 function App() {
 
   return (
-    <>
-     <LandingPage/>
-    </>
+    <div className={AppCss.page}>
+        <LandingPage/>
+    </div>
+     
+    
   )
 }
 
