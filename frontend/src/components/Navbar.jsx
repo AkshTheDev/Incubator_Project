f90ec60fc574ba5bb96ee90bc9813c7f7c46f999
Akshansh Sinha <sinha.akshansh.08@gmail.com>
2025-02-20 17:31:15 +0530

    Header fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,26 +2,24 @@ import NavbarCSS from "../components/Navbar.module.css";
 import arrow from "../images/arrow.svg";
 import logo from "../images/logo.svg";
 function Navbar() {
+  const links = ["About", "Pricing", "Support"];
   return (
     <div className={NavbarCSS.header}>
-        <div>
-             <a href="./blank">
-                <img src={logo} alt="logo was here" className={NavbarCSS.logo}/>
-            </a>
-        </div>
-     
+      <div>
+        <a href="./blank">
+          <img src={logo} alt="logo was here" className={NavbarCSS.logo} />
+        </a>
+      </div>
 
       <nav className={NavbarCSS.navcontainer}>
         <ul className={NavbarCSS.listbox}>
-          <div className={NavbarCSS.listshadow}>
-            <li>About</li>
-          </div>
-          <div className={NavbarCSS.listshadow}>
-            <li>Pricing</li>
-          </div>
-          <div className={NavbarCSS.listshadow}>
-            <li>Support</li>
-          </div>
+          {links.map((item) => {
+            return (
+              <div className={NavbarCSS.listshadow}>
+                <li>{item}</li>
+              </div>
+            );
+          })}
         </ul>
 
         <div className={NavbarCSS.buttonbox}>
