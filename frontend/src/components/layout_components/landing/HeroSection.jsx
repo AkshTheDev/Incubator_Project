de1b5c1365bacd614607991f5442b5725dcfd260
Akshansh Sinha <sinha.akshansh.08@gmail.com>
2025-02-24 00:54:06 +0530

    made section 2

--- frontend/src/components/layout_components/landing/HeroSection.jsx
+++ frontend/src/components/layout_components/landing/HeroSection.jsx
@@ -1,9 +1,23 @@
-import HeroSectionCSS from './HeroSection.module.css'
-function HeroSection(){
-    return(
-        <section className={HeroSectionCSS.featurepage}>
-
-        </section>
-    )
+import HeroSectionCSS from "./HeroSection.module.css";
+function HeroSection() {
+  return (
+    <section className={HeroSectionCSS.featurepage}>
+      <span
+        style={{
+          fontWeight: "bolder",
+          fontSize: "80px",
+          fontFamily: "Audrey, sans-serif",
+          paddingTop: "15rem",
+        }}
+      >
+        Write Award-Winning Scripts
+      </span>
+      <div className={HeroSectionCSS.button}>
+        <a href="/blank">
+            Get Started
+        </a>
+      </div>
+    </section>
+  );
 }
-export default HeroSection
+export default HeroSection;
