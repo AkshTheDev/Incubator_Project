9ccbef1e2b8266112531357cda8782a68a804f5f
Akshansh Sinha <sinha.akshansh.08@gmail.com>
2025-03-25 21:14:36 +0530

    made selectioncard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home'
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Editor from "./pages/Editor";
+import SelectionCard from "./components/layout_components/SelectionCard";
 
 function App() {
   return(
@@ -16,7 +17,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/Editor" element={<Editor/>}/>
-      <Route path="/Editor" element={<Editor/>}/>
+      <Route path="/sel" element={<SelectionCard/>}/>
     </Routes>
   </Router>
   );
---
+++ frontend/src/components/layout_components/SelectionCard.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import SelectionCardCSS from  './SelectionCardCSS.module.css'
+import { useState } from 'react';
+
+
+function SelectionCard() {
+    const contents = [
+        {   
+            id: 1,
+            icon: "fa-solid fa-image",
+            command: 'ctrl+1',
+            name: 'Scene Heading'
+        },
+        {   
+            id: 2,
+            icon: "fa-solid fa-person-running",
+            command: 'ctrl+2',
+            name: 'Action'
+        },
+        {   
+            id: 3,
+            icon: "fa-solid fa-user",
+            command: 'ctrl+3',
+            name: 'Character'
+        },
+        {   
+            id: 4,
+            icon: "fa-solid fa-person-running",
+            command: 'ctrl+4',
+            name: 'Parenthetical'
+        },
+        {   
+            id: 5,
+            icon: "fa-solid fa-person-running",
+            command: 'ctrl+5',
+            name: 'Dialouge'
+        },
+        {   
+            id: 6,
+            icon: "fa-solid fa-person-running",
+            command: 'ctrl+6',
+            name: 'Shoot'
+        },
+        {   
+            id: 7,
+            icon: "fa-solid fa-person-running",
+            command: 'ctrl+7',
+            name: 'Transition'
+        }
+    ]
+
+  return (
+    <div className={SelectionCardCSS.parent}>
+        <div className={SelectionCardCSS.card}>
+            {
+                contents.map((editor)=>{
+                    return(
+                        
+                        <div className={SelectionCardCSS.cardelement} key={editor.id} >
+                           <i class={editor.icon} style={{color: 'white'}}></i>
+                            <div style={{color: 'white'}}>{editor.name}</div>
+                            <div style={{color: 'white'}}>{editor.command}</div>
+
+                        </div>
+                        
+                    )
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default SelectionCard
