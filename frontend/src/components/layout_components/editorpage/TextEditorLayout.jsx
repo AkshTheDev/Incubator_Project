4405a219fb50b5361bbff2b1fb21464349dfae89
Sakshi <sakshishah049@gmail.com>
2025-04-05 18:38:20 +0530

    navbar of mainpage

---
+++ frontend/src/components/layout_components/editorpage/TextEditorLayout.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./TextEditorLayout.module.css";
+import  HomeIcon from"../../../assets/images/HomeIcon.svg"
+import search from "../../../assets/images/SearchIcon.svg"
+import bell from "../../../assets/images/NotificationBell.svg"
+
+export default function TextEditorLayout() {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.navLinks}>
+        <Link to="/Desktop">
+            <img className={styles.icon} src={HomeIcon} alt="home icon"></img>
+        </Link>
+        <Link to="/Search">
+            <img className={styles.icon} src={search} alt="search icon" ></img>
+        </Link>
+        
+        <Link to="/Notification">
+            <img className={styles.icon} src = {bell} alt="Notification icon" ></img>
+        </Link>
+      </div>
+      <Link to="/CreateScript">
+        <button className={styles.navButton}>+Create</button>
+      </Link>
+    </div>
+  );
+}
